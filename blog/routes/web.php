5631d77966cbecd5308f139f06915425bda7c49c
Phuong <phuong.onngoc.mulodo@gmail.com>
2017-09-26 05:26:06 -0400

    lesson 11 Form request data and CSRF

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -33,8 +33,10 @@ Route::get('tasks/{task}', 'TasksController@show');*/
 });*/
 
 Route::get('/', 'PostsController@index');
-Route::get('/posts/{post}', 'PostsController@show');
+//Route::get('/posts/{post}', 'PostsController@show');
+Route::get('/posts/create', 'PostsController@create');
 
+Route::post('/posts', 'PostsController@store');
 //Controller => PostsController
 //Eloquent Model => Post
 //migration => create.posts.table
