ce883b71cb95a6ea1c3fa81bae779a0d22b2ffe6
Phuong <phuong.onngoc.mulodo@gmail.com>
2017-09-26 05:26:06 -0400

    lesson 11 Form request data and CSRF

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Post;
+
 class PostsController extends Controller
 {
     public function index()
@@ -14,5 +16,31 @@ class PostsController extends Controller
     public function show()
     {
       return view('posts.show');
+    }
+
+    public function create()
+    {
+      return view('posts.create');
+    }
+
+    public function store()
+    {
+      //dd(request(['title', 'body']));
+      // Create a new post using the request data
+      //$post = new Post;
+
+      //$post->title = request('title');
+      //$post->body = request('body');
+
+      // Save it to the database
+      //$post->save();
+      Post::create(request(['title', 'body']));
+      /*Post::create([
+        'title' => request('title'),
+        'body' => request('body')
+      ]);*/
+
+      // And then redirect to the home page
+      return redirect('/');
     }
 }
--- blog/app/Http/Controllers/TasksController.php
+++ blog/app/Http/Controllers/TasksController.php
@@ -1,10 +1,7 @@
 <?php
-
-namespace App\Http\Controllers;
-
+/*namespace App\Http\Controllers;
 //use Illuminate\Http\Request;
 use App\Task;
-
 class TasksController extends Controller
 {
     public function index()
@@ -12,11 +9,92 @@ class TasksController extends Controller
       $tasks = Task::all();
       return view('tasks.index', compact('tasks'));
     }
-
-    public function show(/*$id*/Task $task) //Task::find(wildcard)
+    public function show(<$id>Task $task) //Task::find(wildcard)
     {
       //$task = Task::find($id);
       //return $task;
       return view('tasks.show', compact('task'));
+    }
+}*/
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TasksController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        // /tasks
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        // /tasks/create
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        // POST /tasks
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        // GET /tasks/id
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        // GET /tasks/id/edit
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        // PATCH /tasks/id
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        // DELETE /tasks/id
     }
 }
