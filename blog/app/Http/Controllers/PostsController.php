442c2a6d16a8f622e9d890e12d8619eaf20b3b8d
Phuong <phuong.onngoc.mulodo@gmail.com>
2017-09-27 04:03:52 -0400

    lesson 13 Rendering posts

--- blog/app/Http/Controllers/PostsController.php
+++ blog/app/Http/Controllers/PostsController.php
@@ -10,12 +10,15 @@ class PostsController extends Controller
 {
     public function index()
     {
-      return view('posts.index');
+      //$posts = Post::all();
+      $posts = Post::orderBy('created_at', 'desc')->get();
+      return view('posts.index', compact('posts'));
     }
 
-    public function show()
+    public function show(Post $post)
     {
-      return view('posts.show');
+      //$post = Post::find($id);
+      return view('posts.show', compact('post'));
     }
 
     public function create()
--- blog/routes/web.php
+++ blog/routes/web.php
@@ -33,9 +33,8 @@ Route::get('tasks/{task}', 'TasksController@show');*/
 });*/
 
 Route::get('/', 'PostsController@index');
-//Route::get('/posts/{post}', 'PostsController@show');
 Route::get('/posts/create', 'PostsController@create');
-
+Route::get('/posts/{post}', 'PostsController@show');
 Route::post('/posts', 'PostsController@store');
 //Controller => PostsController
 //Eloquent Model => Post
