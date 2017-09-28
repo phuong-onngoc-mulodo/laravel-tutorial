ef2a727b3a831fa15f7d28c93789dbe734adbd8d
Phuong <phuong.onngoc.mulodo@gmail.com>
2017-09-28 03:16:27 -0400

    lesson 16 Add comments

--- blog/routes/web.php
+++ blog/routes/web.php
@@ -36,6 +36,7 @@ Route::get('/', 'PostsController@index');
 Route::get('/posts/create', 'PostsController@create');
 Route::get('/posts/{post}', 'PostsController@show');
 Route::post('/posts', 'PostsController@store');
+Route::post('/posts/{post}/{comment}', 'CommentsController@store');
 //Controller => PostsController
 //Eloquent Model => Post
 //migration => create.posts.table
